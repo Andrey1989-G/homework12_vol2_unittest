7bf8c80678ff9307f91e76846a55abe299439899
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-08-12 22:01:29 +0500

    make order

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -14,4 +14,3 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.my_slice([], 0, "test"), [])
         self.assertEqual(arrs.my_slice([1, 2, 3], -4, 3), [1, 2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], -2, 3), [1, 2, 3])
-
